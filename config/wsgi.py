e00c5471f87421cd0f311b8955a52716939036a8
Joo Hyung <yspac123@gmail.com>
2022-10-11 16:00:05 +0900

    FEAT DB models

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Capstone_Design_1_Image_Editor project.
+WSGI config for config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "Capstone_Design_1_Image_Editor.settings"
+    "DJANGO_SETTINGS_MODULE", "config.settings"
 )
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "Capstone_Design_1_Image_Editor.settings"
+        "DJANGO_SETTINGS_MODULE", "config.settings"
     )
     try:
         from django.core.management import execute_from_command_line
