d650416b454e07132186c29d26ffdc69955619c2
pestyswami <112605911+pestyswami@users.noreply.github.com>
2022-10-11 16:17:55 +0900

    FEAT super user and settings (#5)

--- image_editor/admin.py
+++ image_editor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['user_id']
+
+admin.site.register(User, UserAdmin)
