f96f0f99860d8bdd981ed51119bc7b361638301a
Joo Hyung <yspac123@gmail.com>
2022-10-15 19:53:32 +0900

    Fix url mapping

--- image_editor/urls.py
+++ image_editor/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'image_editor'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:user_id>/', views.detail),
+    path('', views.index, name='index'),
+    path('<int:user_id>/', views.detail, name='detail'),
 ]
