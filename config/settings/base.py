4bcfb3c28226ae15aa14d378ca0e0900555c2c53
goragoraki <kanaba5@naver.com>
2022-10-19 15:07:57 +0900

    logging setting

--- config/settings/base.py
+++ config/settings/base.py
@@ -127,8 +127,70 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# ë¡œê·¸ì¸ ì„±ê³µí›„ ì´ë™í•˜ëŠ” URL
+# ë¡œê·¸?¸ ?„±ê³µí›„ ?´?™?•˜?Š” URL
 LOGIN_REDIRECT_URL = '/'
 
-# ë¡œê·¸ì•„ì›ƒì‹œ ì´ë™í•˜ëŠ” URL
+# ë¡œê·¸?•„?›ƒ?‹œ ?´?™?•˜?Š” URL
 LOGOUT_REDIRECT_URL = '/'
+
+
+# setting logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[{server_time}] {message}',
+            'style': '{',
+        },
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'file': {
+            'level': 'INFO',
+            'filters': ['require_debug_false'],
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': BASE_DIR / 'logs/mysite.log',
+            'maxBytes': 1024*1024*5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'mail_admins', 'file'],
+            'level': 'INFO',
+        },
+        'django.server': {
+            'handlers': ['django.server'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    }
+}
