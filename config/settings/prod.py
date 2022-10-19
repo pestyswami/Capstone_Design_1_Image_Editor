daeab9e3fd13f2445fe664fcd559a2164d4d36f1
Joo Hyung <yspac123@gmail.com>
2022-10-19 20:41:39 +0900

    FEAT domain host connect

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['13.125.173.195']
+ALLOWED_HOSTS = ['13.125.173.195', 'imageeditorcapstone.tk']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG=False
